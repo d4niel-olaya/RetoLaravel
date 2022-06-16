a86115abec09d5cc217774deaf49b415138d734b
d4niel <olayajuandaniel8@gmail.com>
2022-06-15 19:46:55 -0500

    Se agrega el proyecto

---
+++ app/Http/Controllers/ControllerEmployees.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class ControllerEmployees extends Controller
+{
+    public function index(){
+        $listado = Employee::get();
+        return view('showEmployees', ['listado' => $listado]);
+    }
+
+    public function Insertar(Request $request){
+        $emp = new Employee();
+        $nombre = $request -> nombre;
+        $apellido =$request -> apellido;
+        $sexo = $request-> sexo;
+        $emp -> nombre = $nombre;
+        $emp -> apellido = $apellido;
+        $emp -> sexo = $sexo;
+        $emp -> save();
+        if($emp -> save()){
+            $estado = 'ok';
+        }
+        else{
+            $estado = 'no';
+        }
+        return view('createEmployee', ['state'=>$estado]);
+    }
+}
